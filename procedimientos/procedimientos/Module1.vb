bfdba712daf82abf63101b45b2577d071ac115bc
Gzalo Barthe <gzalo1590@hotmail.com>
2018-05-21 20:40:59 -0300

    ejer 3.1

---
+++ procedimientos/procedimientos/Module1.vb
@@ -0,0 +1,16 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim fe As Date = Now
+        fecha(fe)
+        hora(fe)
+    End Sub
+    Private Sub fecha(hoy)
+
+        Console.WriteLine("{0}/{1}/{2}", hoy.Day, hoy.Month, hoy.Year)
+    End Sub
+    Private Function hora(hoy)
+        Console.WriteLine("hora: {0}:{1}", hoy.Hour, hoy.Minute)
+        Return (0)
+    End Function
+End Module
