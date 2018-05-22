1c7378a535b09a23110f276d45b82e1a6163d134
Gzalo Barthe <gzalo1590@hotmail.com>
2018-05-21 21:01:03 -0300

    3.2

---
+++ procedimientos/procedimientos/3_02_suma_dias.vb
@@ -0,0 +1,17 @@
+﻿Module _3_02_suma_dias
+    Sub main()
+        Dim fecha As Date
+        Dim dias As Integer
+        Console.WriteLine("ingrese una fecha")
+        fecha = Console.ReadLine()
+        Console.WriteLine("ingrese una cantidad de dias a sumar")
+        dias = Console.ReadLine()
+        fecha = Sumar(fecha, dias)
+        Console.WriteLine("nueva fecha: " & fecha)
+    End Sub
+    Private Function Sumar(fe As Date, d As Integer) As Date
+
+        Return (fe.AddDays(d))
+    End Function
+
+End Module
